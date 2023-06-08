9edc0647ba4601c12d9fae81fdaac6a5566ec987
Iryna Sokolovska <sokirina777@gmail.com>
2023-06-08 11:52:24 +0300

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+const { email, password }  = event.currentTarget.elements;
+
+  let dataForm;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  } else {
+    dataForm = {
+      [email.name]: email.value,
+      [password.name]: password.value,
+    };
+  }
+  console.log(dataForm);
+  event.currentTarget.reset();
+});
 
